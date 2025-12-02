518c03a0c4952394996c753ec90a84c6d512ca7e
Omni <oscarforbes821@gmail.com>
2025-12-02 18:47:40 +0000

    added .env

--- dataset.py
+++ dataset.py
@@ -1,24 +1,29 @@
 import requests
 import json
 import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+api_key = os.getenv("OPENROUTER_API_KEY")
+api_key = api_key.strip()
 
 printOutput = False
-
 query = "hey"
 
 response = requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
-        "Authorization": "Bearer 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",
+        "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json",
     },
     json={
-        "model": "google/gemma-3n-e4b-it:free",
+        "model": "google/gemma-3n-e4b-it",
         "messages": [
             {
                 "role": "user",
-                # "content": "What is the probability we will reach AGI by the end of 2027? Estimate as a percentage."
-                "content": query
+                "content": "What is the probability we will reach AGI by the end of 2027? Estimate as a percentage."
+                # "content": query
             }
         ],
         "reasoning": {
@@ -27,7 +32,7 @@ response = requests.post(
     }
 )
 
-response.raise_for_status()
+# response.raise_for_status()
 
 data = response.json()
 output = data["choices"][0]["message"]
@@ -39,11 +44,11 @@ with open('DatasetTest.json', 'w', encoding='utf-8') as json_file:
     json.dump({"query": query, "output": StdOutput}, json_file, ensure_ascii=False, indent=2)
 
 if printOutput:
-    print("\n\n Outputting reasoning" \
+    print("\n\n Outputting Reasoning" \
     "-------------------------------------------------------------------------- \n\n")
     time.sleep(2)
     print(CoTOutput)
-    time.sleep(3.6)
+    time.sleep(3)
     print(" \n\n Outputting Content" \
     "-------------------------------------------------------------------------- \n\n")
     time.sleep(2)
