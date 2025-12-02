1f7366ef47b77fedca163219953273a7ec0b1506
Omni <oscarforbes821@gmail.com>
2025-12-02 17:27:36 +0000

    initial commit

---
+++ dataset.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import time
+import csv
+
+printOutput = False
+
+response = requests.post(
+    url="https://openrouter.ai/api/v1/chat/completions",
+    headers={
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json",
+    },
+    json={
+        "model": "google/gemma-3n-e4b-it:free",
+        "messages": [
+            {
+                "role": "user",
+                # "content": "What is the probability we will reach AGI by the end of 2027? Estimate as a percentage."
+                "content": "hey"
+            }
+        ],
+        "reasoning": {
+            "effort": "high", 
+        }
+    }
+)
+
+
+data = response.json()
+output = data["choices"][0]["message"]
+CoTOutput = output.get("reasoning")
+StdOutput = output.get("content")
+
+with open('DatasetTest.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow([StdOutput]) 
+
+if printOutput:
+    print("\n\n Outputting reasoning" \
+    "-------------------------------------------------------------------------- \n\n")
+    time.sleep(2)
+    print(CoTOutput)
+    time.sleep(3.6)
+    print(" \n\n Outputting Content" \
+    "-------------------------------------------------------------------------- \n\n")
+    time.sleep(2)
+    print(StdOutput)
